84131dd1443365f5e7ea0e8b9d87482b8ec83e96
Ndolo7 <imanindolo77@gmail.com>
2026-05-25 23:53:16 +0300

    feat: add products trail report route

--- reports/urls.py
+++ reports/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("reports/suppliers/", views.supplier_report_view, name="supplier-report"),
     path("reports/suppliers/invoice-items/", views.supplier_invoice_items_modal_view, name="supplier-invoice-items-modal"),
     path("reports/orders/", views.orders_report_view, name="orders-report"),
+    path("reports/products-trail/", views.products_trail_report_view, name="products-trail-report"),
     path("reports/shifts/", views.shift_report_view, name="shift-report"),
 ]
